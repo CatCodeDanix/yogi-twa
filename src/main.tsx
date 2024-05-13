4f6beb58cf6cb1732cab573370d3fd00385d83c3
CatCodeDanix <abdolidanial@gmail.com>
2024-05-13 20:01:46 +0330

    project init

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { SDKProvider, type SDKInitOptions } from '@tma.js/sdk-react';
 import App from './App.tsx';
 import './index.css';
 
 import { NextUIProvider } from '@nextui-org/react';
 
+const options: SDKInitOptions = {
+  acceptCustomStyles: true,
+  cssVars: true,
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <SDKProvider options={options}>
+        <App />
+      </SDKProvider>
     </NextUIProvider>
   </React.StrictMode>,
 );
