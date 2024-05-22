a19a6fcc53eac76a5f9d1c0ce802d8369720bffa
CatCodeDanix <abdolidanial@gmail.com>
2024-05-22 11:06:16 +0330

    feat: testing new version of tma.js

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { SDKProvider, type SDKInitOptions } from '@tma.js/sdk-react';
+import { SDKProvider } from '@tma.js/sdk-react';
 import App from './App.tsx';
 import './index.css';
 import { NextUIProvider } from '@nextui-org/react';
@@ -23,15 +23,10 @@ const router = createBrowserRouter([
   },
 ]);
 
-const options: SDKInitOptions = {
-  acceptCustomStyles: true,
-  cssVars: true,
-};
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <NextUIProvider>
-      <SDKProvider options={options}>
+      <SDKProvider acceptCustomStyles debug>
         <RouterProvider router={router} />
       </SDKProvider>
     </NextUIProvider>
